56a443b8a0256359c420ad870ec41c70379589bf
Gannaram Madhavan <108622382+madhavan625@users.noreply.github.com>
2025-08-15 19:14:13 +0530

    Update temp.py

--- temp.py
+++ temp.py
@@ -1,8 +1,6 @@
 import os
 from flask import Flask, render_template, request, jsonify
 from openai import OpenAI
-from dotenv import load_dotenv
-load_dotenv()
 
 app = Flask(__name__)
 
@@ -30,3 +28,4 @@ def submit():
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port, debug=True)
+
